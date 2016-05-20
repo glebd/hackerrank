a2becb7575a6c4d1131da943480221f08d111a94
Gleb Dolgich <glebd70@gmail.com>
2016-05-20 01:30:59 +0100

    Add template code from HackerRank

--- algorithms/warmup/diagonal-difference/main.cpp
+++ algorithms/warmup/diagonal-difference/main.cpp
@@ -1,7 +1,18 @@
+// https://www.hackerrank.com/challenges/diagonal-difference
+
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main() {
-  cout << "Hello, World!" << endl;
+  int n;
+  cin >> n;
+  vector<vector<int>> a(n, vector<int>(n));
+  for (int a_i = 0; a_i < n; a_i++) {
+    for (int a_j = 0; a_j < n; a_j++) {
+      cin >> a[a_i][a_j];
+    }
+  }
   return 0;
 }
