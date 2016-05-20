0e269d4422900d25ab4cb0da6a84594b1700eff7
Gleb Dolgich <glebd70@gmail.com>
2016-05-20 01:46:35 +0100

    Solve challenge

--- algorithms/warmup/diagonal-difference/main.cpp
+++ algorithms/warmup/diagonal-difference/main.cpp
@@ -11,6 +11,35 @@
 
 using namespace std;
 
+/**
+ * Calculates absolute difference between sums of diagonals of a square matrix
+ * @param matrix Input square matrix
+ * @return Absolute difference of sums of diagonals of @a matrix
+ */
+int diagonal_diff(const vector<vector<int>>& matrix) {
+  int diag1 = 0;
+  for (size_t i = 0; i < matrix.size(); ++i) {
+    diag1 += matrix[i][i];
+  }
+  int diag2 = 0;
+  for (size_t i = 0; i < matrix.size(); ++i) {
+    diag2 += matrix[i][matrix.size() - i - 1];
+  }
+  return abs(diag2 - diag1);
+}
+
+#ifdef TEST
+
+TEST_CASE("Sample 1") {
+  vector<vector<int>> input{{11, 2, 4},
+                            {4,  5, 6},
+                            {10, 8, -12}};
+  auto actual = diagonal_diff(input);
+  CHECK(actual == 15);
+}
+
+#else
+
 int main() {
   size_t n;
   cin >> n;
@@ -20,5 +49,9 @@ int main() {
       cin >> a[a_i][a_j];
     }
   }
+  auto result = diagonal_diff(a);
+  cout << result << endl;
   return 0;
 }
+
+#endif
