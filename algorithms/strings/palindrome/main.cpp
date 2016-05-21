71215eeaff135cf6b85766c1e0270f01ab11c717
Gleb Dolgich <glebd70@gmail.com>
2016-05-21 20:43:02 +0100

    Implement is_palindrome()

--- algorithms/strings/palindrome/main.cpp
+++ algorithms/strings/palindrome/main.cpp
@@ -6,8 +6,13 @@
 
 using namespace std;
 
+// Run time complexity: O(n)
+// Stack space complexity: O(log n)
 bool is_palindrome(const string& s) {
-  return false;
+  if (s.length() < 2) return true;
+  if (s[0] != s[s.length() - 1]) return false;
+  auto mid = s.substr(1, s.length() - 2);
+  return is_palindrome(mid);
 }
 
 TEST_CASE("a") {
@@ -20,4 +25,12 @@ TEST_CASE("motor") {
 
 TEST_CASE("rotor") {
   CHECK(is_palindrome("rotor"));
+}
+
+TEST_CASE("macadam") {
+  CHECK(!is_palindrome("macadam"));
+}
+
+TEST_CASE("naan") {
+  CHECK(is_palindrome("naan"));
 }
