bdc3a52e1baf99b5a9a56e512286597666413131
Gleb Dolgich <glebd70@gmail.com>
2016-05-30 00:46:42 +0100

    Add failing tests

--- algorithms/warmup/time-conversion/main.cpp
+++ algorithms/warmup/time-conversion/main.cpp
@@ -14,6 +14,14 @@ string miltime(string time) {
 
 #ifdef TEST
 
+TEST_CASE("AM") {
+  CHECK(miltime("07:05:45AM") == "07:05:45");
+}
+
+TEST_CASE("PM") {
+  CHECK(miltime("07:05:45PM") == "19:05:45");
+}
+
 #else
 
 int main() {
