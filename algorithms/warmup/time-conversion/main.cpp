02102b995015ee15ccbd259bc0cc93ab0d730346
Gleb Dolgich <glebd70@gmail.com>
2016-05-30 00:44:48 +0100

    Set up tests

--- algorithms/warmup/time-conversion/main.cpp
+++ algorithms/warmup/time-conversion/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+
+#define TEST
+
+#ifdef TEST
+#include "catch.hpp"
+#endif
+
 using namespace std;
+
+#ifdef TEST
+
+#else
 
 int main() {
   cout << "Hello, World!" << endl;
   return 0;
 }
+
+#endif
