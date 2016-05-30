9d51df80c7623495d467adfe44e39b4c98797a85
Gleb Dolgich <glebd70@gmail.com>
2016-05-30 01:15:38 +0100

    Handle basic cases

--- algorithms/warmup/time-conversion/main.cpp
+++ algorithms/warmup/time-conversion/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 
 #define TEST
 
@@ -8,8 +10,23 @@
 
 using namespace std;
 
+/**
+ * Converts AM/PM time to 24-hour time
+ * @param time Time string in the format "hh:mm:ss{AM|PM}"
+ * @return Time string in 24-hour format "hh:mm:ss"
+ */
 string miltime(string time) {
-  return time;
+  if (time.length() != 10) return time;
+  string h = time.substr(0, 2);
+  string m = time.substr(3, 2);
+  string s = time.substr(6, 2);
+  string a = time.substr(8, 1);
+  int hour = atoi(h.c_str());
+  if (a == "P") hour += 12;
+  ostringstream os;
+  os.fill('0');
+  os << setw(2) << hour << ":" << m << ":" << s;
+  return os.str();
 }
 
 #ifdef TEST
