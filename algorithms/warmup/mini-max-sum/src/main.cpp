1a701ee0fbc7486e5dbf6985c3ee5ce546274dfc
Gleb Dolgich <glebd70@gmail.com>
2022-08-17 22:21:50 +0100

    Add skeleton code from hackerrank

--- algorithms/warmup/mini-max-sum/src/main.cpp
+++ algorithms/warmup/mini-max-sum/src/main.cpp
@@ -1,9 +1,79 @@
-#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
-    cout << "hello world!" << endl;
-    return 0;
+string ltrim(const string &str) {
+  string s(str);
+
+  s.erase(
+      s.begin(),
+      find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+  );
+
+  return s;
 }
+
+string rtrim(const string &str) {
+  string s(str);
+
+  s.erase(
+      find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+      s.end()
+  );
+
+  return s;
+}
+
+vector<string> split(const string &str) {
+  vector<string> tokens;
+
+  string::size_type start = 0;
+  string::size_type end = 0;
+
+  while ((end = str.find(" ", start)) != string::npos) {
+    tokens.push_back(str.substr(start, end - start));
+
+    start = end + 1;
+  }
+
+  tokens.push_back(str.substr(start));
+
+  return tokens;
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void miniMaxSum(vector<int> arr) {
+
+}
+
+#if 0
+int main()
+{
+
+  string arr_temp_temp;
+  getline(cin, arr_temp_temp);
+
+  vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+  vector<int> arr(5);
+
+  for (int i = 0; i < 5; i++) {
+    int arr_item = stoi(arr_temp[i]);
+
+    arr[i] = arr_item;
+  }
+
+  miniMaxSum(arr);
+
+  return 0;
+}
+#endif
