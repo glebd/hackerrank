1efcf602a96172689f51b6dab941a0bfa441c969
Gleb Dolgich <glebd70@gmail.com>
2022-08-18 20:51:02 +0100

    Reformat code

--- algorithms/warmup/mini-max-sum/src/main.cpp
+++ algorithms/warmup/mini-max-sum/src/main.cpp
@@ -6,43 +6,42 @@
 
 using namespace std;
 
-string ltrim(const string &str) {
-  string s(str);
+string ltrim(const string &str)
+{
+    string s(str);
 
-  s.erase(
-      s.begin(),
-      find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-  );
+    s.erase(s.begin(), find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
 
-  return s;
+    return s;
 }
 
-string rtrim(const string &str) {
-  string s(str);
+string rtrim(const string &str)
+{
+    string s(str);
 
-  s.erase(
-      find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-      s.end()
-  );
+    s.erase(find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(), s.end());
 
-  return s;
+    return s;
 }
 
-vector<string> split(const string &str) {
-  vector<string> tokens;
+vector<string> split(const string &str)
+{
+    vector<string> tokens;
 
-  string::size_type start = 0;
-  string::size_type end = 0;
+    string::size_type start = 0;
+    string::size_type end = 0;
 
-  while ((end = str.find(" ", start)) != string::npos) {
-    tokens.push_back(str.substr(start, end - start));
+    while ((end = str.find(" ", start)) != string::npos)
+    {
+        tokens.push_back(str.substr(start, end - start));
 
-    start = end + 1;
-  }
+        start = end + 1;
+    }
 
-  tokens.push_back(str.substr(start));
+    tokens.push_back(str.substr(start));
 
-  return tokens;
+    return tokens;
+
 }
 
 /*
@@ -51,8 +50,8 @@ vector<string> split(const string &str) {
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
 
-void miniMaxSum(vector<int> arr) {
-
+void miniMaxSum(vector<int> arr)
+{
 }
 
 #if 0
